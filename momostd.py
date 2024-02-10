a3a0c9f0e951e075af3bc41e256d6b6e62bddcdf
Muhammed Abdel Hamid Shelleh <muhammedshelleh@gmail.com>
2024-02-10 17:19:00 -0500

    x

--- momostd.py
+++ momostd.py
@@ -9,6 +9,8 @@ import logging
 from messages import Upload, Request
 from util import even_split
 from peer import Peer
+import heapq
+
 
 class LessDummy(Peer):
     def post_init(self):
@@ -17,66 +19,78 @@ class LessDummy(Peer):
         # Declare any variables here that you want to be able to access in future rounds #
         ##################################################################################
 
-        #This commented out code is and example of a python dictionsary,
-        #which is a convenient way to store a value indexed by a particular "key"
-        #self.dummy_state = dict()
-        #self.dummy_state["cake"] = "lie"
-    
+        # This commented out code is and example of a python dictionsary,
+        # which is a convenient way to store a value indexed by a particular "key"
+        # self.dummy_state = dict()
+        class LessDummy(Peer):
+            def post_init(self):
+                print("post_init(): %s here!" % self.id)
+
+                # Declare any variables here that you want to be able to access in future rounds
+                self.dummy_state = dict()
+                self.dummy_state["cake"] = "lie"
+                self.frequencies = {}
+                self.pieces = []
+                self.conf = None  # Configuration object for the client
+                self.max_requests = (
+                    0  # Maximum number of requests to send to other peers
+                )
+                self.available_pieces = (
+                    set()
+                )  # Set to store the pieces available to the peer
+                self.downloads = []  # List to store the download history of the peer
+                self.uploaded = 0  # Total amount of data uploaded by the peer
+                self.downloaded = 0  # Total amount of data downloaded by the peer
+                self.requests_sent = []  # List to store the requests sent by the peer
+                self.uploads_received = (
+                    []
+                )  # List to store the uploads received by the peer
+                self.choked_by = (
+                    set()
+                )  # Set to store the peers that have choked the peer
+                self.interested_in = (
+                    set()
+                )  # Set to store the peers that the peer is interested in
+                self.optimistic_unchoked = (
+                    None  # Peer that is optimistically unchoked by the peer
+                )
+                self.round = 0  # Current round number
+
+                # Add any additional variables or data structures as needed
+
     def requests(self, peers, history):
-        """
-        peers: available info about the peers (who has what pieces)
-        history: what's happened so far as far as this peer can see
-
-        returns: a list of Request() objects
-
-        This will be called after update_pieces() with the most recent state.
-        """
-        #Calculate the pieces you still need
         needed = lambda i: self.pieces[i] < self.conf.blocks_per_piece
         needed_pieces = list(filter(needed, list(range(len(self.pieces)))))
         # Symmetry breaking is good, it creates more opportunities to trade
         # Try commenting this out and see what happens to performance...
         random.shuffle(needed_pieces)
-        
+
         np_set = set(needed_pieces)  # sets support fast intersection ops.
 
+        logging.debug("%s here: still need pieces %s" % (self.id, needed_pieces))
 
-        logging.debug("%s here: still need pieces %s" % (
-            self.id, needed_pieces))
-
-
-        requests = []   # We'll put all the things we want here
+        requests = []  # We'll put all the things we want here
 
         # count frequencies of all pieces that the other peers have
         # this will be useful for implementing rarest first
         ###########################################################
         # We added this                                           #
         ###########################################################
-        frequencies = {}
+
         for peer in peers:
             for piece in peer.available_pieces:
-                    if piece in frequencies:
-                        frequencies[piece] = frequencies[piece] + 1
-                    else:
-                        frequencies[piece] = 1
-        
+                if piece in self.frequencies:
+                    self.frequencies[piece] += 1
+                else:
+                    self.frequencies[piece] = 1
 
+        self.pieces = [(count, piece) for piece, count in self.frequencies.items()]
+        heapq.heapify(self.pieces)
 
-        # request all available pieces from all peers!
-        # (up to self.max_requests from each)
-        #############################################################################
-        # We adapted this code from dummy to implement rarest first                 #
-        #############################################################################
-        for peer in peers:
-            av_set = set(peer.available_pieces)
-            isect = av_set.intersection(np_set)
-            n = int(min(self.max_requests, len(isect)))
-            # More symmetry breaking -- ask for random pieces when tied.
-            isectlist = list(isect)
-            random.shuffle(isectlist)
-            isectlist.sort(key=lambda p: frequencies[p])
-            for piece_id in isectlist[:n]:
-                # aha! The peer has this piece! Request it.
+        requests = []
+        while len(requests) < self.max_requests and self.pieces:
+            _, piece_id = heapq.heappop(self.pieces)
+            if piece_id in needed_pieces:
                 start_block = self.pieces[piece_id]
                 r = Request(self.id, peer.id, piece_id, start_block)
                 requests.append(r)
@@ -100,8 +114,7 @@ class LessDummy(Peer):
         ##############################################################################
 
         round = history.current_round()
-        logging.debug("%s again.  It's round %d." % (
-            self.id, round))
+        logging.debug("%s again.  It's round %d." % (self.id, round))
         # One could look at other stuff in the history too here.
         # For example, history.downloads[round-1] (if round != 0, of course)
         # has a list of Download objects for each Download to this peer in
@@ -121,15 +134,12 @@ class LessDummy(Peer):
             request = random.choice(requests)
             chosen = [request.requester_id]
 
-
-            
             # Now that we have chosen who to unchoke, the standard client evenly shares
             # its bandwidth among them
             bws = even_split(self.up_bw, len(chosen))
 
         # create actual uploads out of the list of peer ids and bandwidths
         # You don't need to change this
-        uploads = [Upload(self.id, peer_id, bw)
-                   for (peer_id, bw) in zip(chosen, bws)]
-            
+        uploads = [Upload(self.id, peer_id, bw) for (peer_id, bw) in zip(chosen, bws)]
+
         return uploads
