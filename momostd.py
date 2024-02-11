a08cb89100ad17885029dea7132a550e5c33fefa
Muhammed Abdel Hamid Shelleh <muhammedshelleh@gmail.com>
2024-02-10 21:27:42 -0500

    hahhjkwdihc ioqwjdic

--- momostd.py
+++ momostd.py
@@ -93,48 +93,39 @@ class LessDummy(Peer):
         return requests
 
     def uploads(self, requests, peers, history):
-        """
-        requests -- a list of the requests for this peer for this round
-        peers -- available info about all the peers
-        history -- history for all previous rounds
-
-        returns: list of Upload objects.
-
-        In each round, this will be called after requests().
-        """
-
-        ##############################################################################
-        # The code and suggestions here will get you started for the standard client #
-        # You'll need to change things for the other clients                         #
-        ##############################################################################
-
         round = history.current_round()
-        logging.debug("%s again.  It's round %d." % (self.id, round))
-        # One could look at other stuff in the history too here.
-        # For example, history.downloads[round-1] (if round != 0, of course)
-        # has a list of Download objects for each Download to this peer in
-        # the previous round.
+        logging.debug("%s again. It's round %d." % (self.id, round))
 
+        # Initial setup for no requests scenario
         if len(requests) == 0:
             logging.debug("No one wants my pieces!")
             chosen = []
             bws = []
         else:
-            logging.debug("Still here: uploading to a random peer")
-
-            ########################################################################
-            # The dummy client picks a single peer at random to unchoke.           #
-            # You should decide a set of peers to unchoke accoring to the protocol #
-            ########################################################################
-            request = random.choice(requests)
-            chosen = [request.requester_id]
-
-            # Now that we have chosen who to unchoke, the standard client evenly shares
-            # its bandwidth among them
+            # Standard strategy for choosing whom to unchoke
+            logging.debug(
+                "Still here: %s, uploading to a random peer %d" % self.id, chosen
+            )
+            chosen = [
+                req.requester_id
+                for req in random.sample(requests, min(len(requests), 3))
+            ]  # Example: unchoke up to 3 peers based on your strategy
             bws = even_split(self.up_bw, len(chosen))
 
-        # create actual uploads out of the list of peer ids and bandwidths
-        # You don't need to change this
-        uploads = [Upload(self.id, peer_id, bw) for (peer_id, bw) in zip(chosen, bws)]
+            # Optimistic unchoking every N rounds
+            unchoke_int = 3  # For example, every 3 rounds
+            if round % unchoke_int == 0:
+                # Ensure not to optimistically unchoke an already chosen peer
+                available_peers = {
+                    req.requester_id
+                    for req in requests
+                    if req.requester_id not in chosen
+                }
+                if available_peers:
+                    optimistic_unchoke_peer = random.choice(list(available_peers))
+                    chosen.append(optimistic_unchoke_peer)
+                    # Adjust bandwidths after adding an optimistically unchoked peer
+                    bws = even_split(self.up_bw, len(chosen))
 
+        uploads = [Upload(self.id, peer_id, bw) for (peer_id, bw) in zip(chosen, bws)]
         return uploads
